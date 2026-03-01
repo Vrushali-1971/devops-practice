a871665f76892ecc5c1c59bdc8215db33049e723
Vrushali <vrushaliambore3908@gmail.com>
2026-03-01 17:07:20 +0000

    Added system info script

---
+++ scripting/system_info.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "System Information"
+echo "------------------"
+
+echo "current User:"
+whoami
+
+echo "Current Date:"
+date
+
+echo "Disk Usage:"
+df -h
+
+echo "Memory Usage:"
+free -h
+
+echo "Script execution completed"
+
