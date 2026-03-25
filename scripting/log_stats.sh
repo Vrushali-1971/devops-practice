ccf6b4b0b369bc969b31cabec551a7b45517aa20
Vrushali-1971 <vrushaliambore3908@gmail.com>
2026-03-25 18:36:36 +0000

    Fix error count logic

--- scripting/log_stats.sh
+++ scripting/log_stats.sh
@@ -13,4 +13,11 @@ echo "The date is: $(date)"
 
 echo "The number of log files are: $(find "$log_path" -name '*.log' | wc -l)"
 
-echo "The number of Error occurrences are: $(grep -o "Error" "$log_path"/*.log | wc -l)" 
+echo "The number of Error occurrences are: $(grep -c "Error" "$log_path"/error.log)"
+
+echo "Count only users from access.log"
+echo "Users are: $(grep -c "User" "$log_path"/access.log)"
+
+echo "Count only errors from error.log"
+echo "Errors are: $(grep -c "Error" "$log_path"/error.log)"
+
